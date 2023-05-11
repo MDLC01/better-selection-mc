6ba4f606d56c84023eb4942dfc33a43da1e0c88e
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2023-05-12 00:13:39 +0200

    Initial commit

---
+++ src/main/java/com/mdlc/betterselection/CharacterClass.java
@@ -0,0 +1,91 @@
+package com.mdlc.betterselection;
+
+
+/**
+ * Character classes form a partition of the set of all characters.
+ */
+public enum CharacterClass {
+    /**
+     * The Word character class contains all the {@linkplain Character#isAlphabetic(int) letters},
+     * {@linkplain Character#isIdeographic(int) ideographs} and {@linkplain Character#isDigit(char) digits}, as well as
+     * the underscore.
+     */
+    WORD,
+    /**
+     * The Whitespace character class contains all {@linkplain Character#isSpaceChar(char) whitespace characters}.
+     */
+    WHITESPACE,
+    /**
+     * The Punctuation character class contains any character that is not a {@linkplain #WORD word} character nor a
+     * {@linkplain #WHITESPACE whitespace} character.
+     */
+    PUNCTUATION;
+
+    /**
+     * Tests if a character belongs to the Word class.
+     *
+     * @param c
+     *         the character to test
+     * @return {@code true} if, and only if, {@code c} belongs to the Word class
+     * @see #WORD
+     */
+    private static boolean isWordCharacter(char c) {
+        return Character.isAlphabetic(c) || Character.isIdeographic(c) || Character.isDigit(c) || c == '_';
+    }
+
+    /**
+     * Tests if a character belongs to the Whitespace class.
+     *
+     * @param c
+     *         the character to test
+     * @return {@code true} if, and only if, {@code c} belongs to the Whitespace class
+     * @see #WHITESPACE
+     */
+    private static boolean isWhitespaceCharacter(char c) {
+        return Character.isSpaceChar(c);
+    }
+
+    /**
+     * Tests if a character belongs to the Punctuation class.
+     *
+     * @param c
+     *         the character to test
+     * @return {@code true} if, and only if, {@code c} belongs to the Punctuation class
+     * @see #PUNCTUATION
+     */
+    private static boolean isPunctuationCharacter(char c) {
+        return !isWordCharacter(c) && !isWhitespaceCharacter(c);
+    }
+
+    /**
+     * Returns the class of a specific character.
+     *
+     * @param c
+     *         the character to get the class of
+     * @return the character class of {@code c}
+     */
+    public static CharacterClass fromCharacter(char c) {
+        if (isWordCharacter(c)) {
+            return WORD;
+        } else if (isWhitespaceCharacter(c)) {
+            return WHITESPACE;
+        } else {
+            return PUNCTUATION;
+        }
+    }
+
+    /**
+     * Tests if a character belongs to this class.
+     *
+     * @param c
+     *         the character to test
+     * @return {@code true} if, and only if, {@code c} belongs to this class
+     */
+    public boolean contains(char c) {
+        return switch (this) {
+            case WORD -> isWordCharacter(c);
+            case WHITESPACE -> isWhitespaceCharacter(c);
+            case PUNCTUATION -> isPunctuationCharacter(c);
+        };
+    }
+}
---
+++ src/main/java/com/mdlc/betterselection/mixin/EditBoxMixin.java
@@ -0,0 +1,116 @@
+package com.mdlc.betterselection.mixin;
+
+import com.mdlc.betterselection.CharacterClass;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+
+@Mixin(EditBox.class)
+public abstract class EditBoxMixin extends AbstractWidget {
+    private EditBoxMixin(int x, int y, int width, int height, Component message) {
+        super(x, y, width, height, message);
+    }
+
+    @Shadow @Final private Font font;
+    @Shadow private String value;
+    @Shadow private int displayPos;
+
+    @Shadow
+    public abstract void moveCursorTo(int i);
+
+
+    /**
+     * Traverses one or multiple words.
+     *
+     * @param directedCount
+     *         the number of words to traverse; negative to the left, positive to the right
+     * @param index
+     *         the initial position of the cursor
+     * @return the index of the position of the cursor after traversing the words
+     */
+    private int traverseWord(int directedCount, int index) {
+        int direction = directedCount > 0 ? 1 : -1;
+        int readOffset = directedCount > 0 ? 0 : -1;
+        int count = Mth.abs(directedCount);
+
+        if (index == 0 && direction == -1 || index == this.value.length() && direction == 1) {
+            return index;
+        }
+
+        for (int i = 0; i < count; i++) {
+            // Words are separated by at least one whitespace
+            if (Character.isSpaceChar(this.value.charAt(index + readOffset))) {
+                index += direction;
+            }
+            if (index == 0 && direction == -1 || index == this.value.length() && direction == 1) {
+                return index;
+            }
+
+            CharacterClass characterClass = CharacterClass.fromCharacter(this.value.charAt(index + readOffset));
+            do {
+                index += direction;
+                if (index == 0 && direction == -1 || index == this.value.length() && direction == 1) {
+                    return index;
+                }
+            } while (characterClass.contains(this.value.charAt(index + readOffset)));
+        }
+
+        return index;
+    }
+
+    /**
+     * Improves the Vanilla word-by-word cursor movement feature.
+     */
+    @Inject(method = "getWordPosition(IIZ)I", at = @At("HEAD"), cancellable = true)
+    private void onGetWordPosition(int directedCount, int index, boolean skipWhitespaceAfterWord, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(traverseWord(directedCount, index));
+    }
+
+    /**
+     * Tests if the shift key is down instead directly instead of using a cached value that might not be up-to-date.
+     * <p>
+     * Fixes <a href="https://bugs.mojang.com/browse/MC-260563">MC-260563</a>.
+     */
+    @Redirect(method = "moveCursorTo", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/components/EditBox;shiftPressed:Z"))
+    private boolean redirectShiftPressed(EditBox instance) {
+        return Screen.hasShiftDown();
+    }
+
+    /**
+     * When the mouse is clicked over a character, moves the cursor to the closest possible position, instead of always
+     * after the clicked character.
+     */
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @Inject(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/EditBox;moveCursorTo(I)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void modifyCursorPosition(double x, double y, int button, CallbackInfoReturnable<Boolean> cir, boolean isClickWithinBox, int mouseXInBox, String displayedText) {
+        // The prefix is the text between the beginning of the box and the clicked character (excluded)
+        String prefix = this.font.plainSubstrByWidth(displayedText, mouseXInBox);
+        int clickedCharacterIndex = this.displayPos + prefix.length();
+        if (clickedCharacterIndex == this.value.length()) {
+            this.moveCursorTo(clickedCharacterIndex);
+        } else {
+            int prefixWidth = this.font.width(prefix);
+            int clickedCharacterWidth = this.font.width(displayedText.substring(prefix.length(), prefix.length() + 1));
+            int spaceLeft = mouseXInBox - prefixWidth;
+            int spaceRight = prefixWidth + clickedCharacterWidth - mouseXInBox;
+            if (spaceRight > spaceLeft) {
+                this.moveCursorTo(clickedCharacterIndex);
+            } else {
+                this.moveCursorTo(clickedCharacterIndex + 1);
+            }
+        }
+        cir.setReturnValue(true);
+    }
+}
