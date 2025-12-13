0fe1742072e6f86213017a1c553ada33f4bbee07
Malo <57839069+MDLC01@users.noreply.github.com>
2025-12-13 14:50:23 +0100

    Update to minecraft 1.21.11

--- src/main/java/com/mdlc/betterselection/mixin/EditBoxMixin.java
+++ src/main/java/com/mdlc/betterselection/mixin/EditBoxMixin.java
@@ -1,12 +1,12 @@
 package com.mdlc.betterselection.mixin;
 
 import com.mdlc.betterselection.WordMachine;
-import net.minecraft.Util;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
